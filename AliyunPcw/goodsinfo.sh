41fdfeedd707c35a2c46fa5efc49d7116d78fe98
ansible-tower <pcwadmin2016@163.com>
2016-12-07 15:32:51 +0800

    add AliyunPcw dir and some scripts

---
+++ AliyunPcw/goodsinfo.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# this script use to auto execute php files , which can generate some excel files in /home/ecshopexcel
+
+curl -i http://www.pcw365.com/ecshop2/goodsinfo.php
+curl -i http://www.pcw365.com/ecshop2/autoconfirm.php
+
---
+++ AliyunPcw/remove_server_log.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# created by carson
+# used to delete expired log
+
+expired=20
+
+
+apache_log_dir=/home/server/apache2/logs
+
+cd $apache_log_dir
+find ./ -name "201*log"  -mtime  +${expired}   -exec rm -f '{}' \;
+
