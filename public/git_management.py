51a0c5339ba6858c3131588eb7f48c50a270c706
ansible-tower <pcwadmin2016@163.com>
2016-12-09 20:40:47 +0800

    correct syntax in git_management.yml

--- public/git_management.py
+++ public/git_management.py
@@ -8,14 +8,14 @@ class git_management(self.op):
 	
 	def __init__(self):
 		pass
-	def pull(self,self.path):  # define git pull function
-		result=commands.getoutput(" cd {directory} && git pull " .format(directory=self.path))
+	def pull(self,path):  # define git pull function
+		result=commands.getoutput(" cd {directory} && git pull " .format(directory=path))
 		return result
-	def get_log(self,self.path): # define git log function
-		result=commands.getoutput(" cd {directory} && git log " .format(directory=self.path))
+	def get_log(self,path): # define git log function
+		result=commands.getoutput(" cd {directory} && git log " .format(directory=path))
 		return result	    # define git reset --hard HEADnumber	
-	def rollback(self,self.path,self.head):
-		result=commands.getoutput(" cd {directory} && git reset --hard {headname} " .format(directory=self.path,headname=self.head))
+	def rollback(self,path,head):
+		result=commands.getoutput(" cd {directory} && git reset --hard {headname} " .format(directory=path,headname=head))
 		return result
 
 
@@ -27,8 +27,10 @@ if operation=="pull":
 	output=do_git.pull(sys.argv[2])
 elif operation=="get_log":
 	output=do_git.pull(sys.argv[2])
-else operation=="rollback":
+elif operation=="rollback":
 	output=do_git.rollbackup(sys.argv[2],sys.argv[3])
+else :
+	pass
 print output
 	
 	 
