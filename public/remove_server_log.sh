c9a035875c294b062c1eea84ee988d07b1c20f57
ansible-tower <pcwadmin2016@163.com>
2016-12-14 15:29:47 +0800

    mov remove_server_log.sh to directory public

--- public/remove_server_log.sh
+++ public/remove_server_log.sh
@@ -8,7 +8,7 @@ expired=30
 apache_log_dir=/home/server/apache2/logs
 
 cd $apache_log_dir
-find ./ -name "201*log"  -mtime  +${expired}   -exec rm -f '{}' \;
+find ./ -name "20*log"  -mtime  +${expired}   -exec rm -f '{}' \;
 
 
 
