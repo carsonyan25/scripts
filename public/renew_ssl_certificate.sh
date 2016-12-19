148f03f4c2f739accda900f9118fe4da74e92910
ansible-tower <pcwadmin2016@163.com>
2016-12-19 16:38:57 +0800

    modify condition  to case style in renew_ssl_certificate.sh

--- public/renew_ssl_certificate.sh
+++ public/renew_ssl_certificate.sh
@@ -2,9 +2,20 @@
 #created by carson
 #renew ssl certificate  automatically (signed to let's encry )
 
+case $1 in 
+	apache)
+		certbot renew --quiet
+		service apache restart
+		;;
+	nginx)
+		service nginx stop 
+		certbot renew --quiet
+		service nginx start 
+		;;
+	*)
+		echo "parameter must be apache or nginx"
+		;;
+esac	
 
-service $1 stop  
-certbot renew --quiet
-service $1 start 
 
 
