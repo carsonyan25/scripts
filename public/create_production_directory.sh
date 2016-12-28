e9f6a86ea561b0f6124ea3042cdd829b202e4c38
ansible-tower <pcwadmin2016@163.com>
2016-12-28 19:07:41 +0800

    drop parameter mode in create_production_directory.sh

--- public/create_production_directory.sh
+++ public/create_production_directory.sh
@@ -2,19 +2,13 @@
 #created by carson on 12/8/2016
 
 dir=$1
-mode=$2 
 
 
 # the directory(path)  exists or not ?
 if [ ! -d "$dir" ] ; then
-	if [ $mode -ne ""  ] ; then 
-		mkdir  -m $mode -p $dir
-		echo " $dir has been created and it's directory structure is \n "
-	else
 		mkdir -p $dir
-
-	echo " $dir has been created and it's directory structure is \n "
-	tree $dir
+		echo " $dir has been created and it's directory structure is"
+		tree $dir
 
 else
 	echo "$dir is exists !"
