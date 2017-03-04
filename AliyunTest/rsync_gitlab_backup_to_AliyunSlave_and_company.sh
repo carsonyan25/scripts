0c36f4fb8b9b05794ccc5d1522f69baa3d86dad8
ansible-tower <pcwadmin2016@163.com>
2017-03-04 22:47:57 +0800

    modify rsync_gitlab_backup_to_AliyunSlave_and_company.sh

--- AliyunTest/rsync_gitlab_backup_to_AliyunSlave_and_company.sh
+++ AliyunTest/rsync_gitlab_backup_to_AliyunSlave_and_company.sh
@@ -6,27 +6,31 @@
 gitlab_src=/var/opt/gilab/backups
 aliyunslave=aliyuntest@139.224.26.247::aliyuntest_data
 company=backup-company@files.pcw365.com::backup-aliyuntest
-passfile=/root/shell/aliyuntest.pass
+aliyuntest_pass=/root/shell/aliyuntest.pass
+backup-company_pass=/root/shell/backup-company.pass
 logfile=/root/shell/rsync_gitlab_backup_to_aliyunslave_and_company.log
 curdate=`date +%Y%m%d`
 gitlab_setting_file=/tmp/gitlab-${curdate}.tar.gz
+gitlab_new_backup=empty
 
 make_backups(){
 	cd /etc/
 	tar -czf ${gitlab_setting_file}  gitlab
 	/usr/bin/gitlab-rake gitlab:backup/create
-	
+	cd $gitlab_src
+	git_backup=`ls -r |head -n 1`
+	git_new_backup=$git_backup-$curdate
+	mv $git_backup $git_new_backup
 }
 
 rsync_backups(){
 	cd /etc/
-	rsync -avpP --quiet --bwlimit=5000 --log-file=${logfile} --password-file=${passfile}	${gitlab_setting_file} $aliyunslave
-	rsync -avpP --quiet --bwlimit=5000 --log-file=${logfile} --password-file=${passfile}	${gitlab_setting_file} $company
+	rsync -avpP --quiet --bwlimit=5000 --log-file=${logfile} --password-file=${aliyuntest_pass}	${gitlab_setting_file} $aliyunslave
+	rsync -avpP --quiet --bwlimit=5000 --log-file=${logfile} --password-file=${backup-company_pass}	${gitlab_setting_file} $company
 	rm -f /tmp/gitlab*.gz
-	cd $gitlab_src 
-	gitlab_new_backup=`ls -r | head -n 1`-${curdate}
-	rsync -avpP --quiet --bwlimit=5000 --log-file=${logfile} --password-file=${passfile}	${gitlab_new_backup} $aliyunslave
-	rsync -avpP --quiet --bwlimit=5000 --log-file=${logfile} --password-file=${passfile}	${gitlab_new_backup} $company
+	cd $gitlab_src
+	rsync -avpP --quiet --bwlimit=5000 --log-file=${logfile} --password-file=${aliyuntest_pass}	${gitlab_new_backup} $aliyunslave
+	rsync -avpP --quiet --bwlimit=5000 --log-file=${logfile} --password-file=${backup-company_pass}	${gitlab_new_backup} $company
 	rm -f $gitlab_new_backup
 }
 
