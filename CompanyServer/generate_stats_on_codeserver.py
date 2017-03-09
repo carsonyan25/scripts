96a1e860100afd6aa6d04a427b2b4b745fdf99d1
ansible-tower <pcwadmin2016@163.com>
2017-03-09 10:59:14 +0800

    add generate total method  in generate_stats_on_codeserver.py

--- CompanyServer/generate_stats_on_codeserver.py
+++ CompanyServer/generate_stats_on_codeserver.py
@@ -40,19 +40,33 @@ def remove_file():
 	result=commands.getstatusoutput("cd /tmp && rm -f cad-*.tar.gz usercoun*.data aec*.form")
 	print result
 
-# curl url to generate statistics	
-def generate_stats():
-	c= pycurl.Curl()
-	c.setopt(c.URL, 'http://test.pcw365.com/disk/statistics/increment.php?validate=cadtest268')
-	c.setopt(c.VERBOSE, True)
-	c.perform()
+# curl url to generate statistics
+class generate_stats:
 
+	def __init__(self):
+		pass
+	
+	def increment(self):
+		c= pycurl.Curl()
+		c.setopt(c.URL, 'http://test.pcw365.com/disk/statistics/increment.php')
+		c.setopt(c.POSTFIELDS,'validate=cadtest268')
+		c.setopt(c.VERBOSE, True)
+		c.perform()
+
+	def total(self):
+		c= pycurl.Curl()
+		c.setopt(c.URL, 'http://test.pcw365.com/disk/statistics/total.php')
+		c.setopt(c.POSTFIELDS,'validate=cadtest268')
+		c.setopt(c.VERBOSE, True)
+		c.perform()
 
 if __name__ == '__main__':
 	copy_file()
 	uncompress()
 	import_stats(curfile="/tmp/" + dest_file,table="usercount_1_170118")
 	remove_file()
-	generate_stats()
+	stats=generate_stats()
+	stats.increment()
+	stats.total()
 else :
 	pass
