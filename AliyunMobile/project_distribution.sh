e68c3f9425c3b07fcca617841116d0b11b028329
ansible-tower <pcwadmin2016@163.com>
2017-03-13 15:04:06 +0800

    add project_distribution.sh

--- AliyunMobile/project_distribution.sh
+++ AliyunMobile/project_distribution.sh
@@ -12,13 +12,18 @@ pull_code(){
 }
 
 update_docker_setting(){
-	/usr/bin/docker -ti --rm -v  $PROJECT_DIR/:/home pwww.pcw365.com:5000/nodejs sh -c 'cd /home && npm install' 
+	/usr/bin/docker -ti --rm -v  $PROJECT_DIR/:/home pwww.pcw365.com:5111/nodejs sh -c 'cd /home && npm install' 
 }
 
 delete_pod(){
 	/usr/bin/kubectl get pods -n project | grep ${1} |awk '{print $1}'|xargs  kubectl delete pods  -n project
 }
 
+if [ $# -eq 0 ] ; then
+	{
+		echo "input the  project name"
+		exit(3)
+	}	 
 pull_code
 update_docker_setting
 delete_pod
