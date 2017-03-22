dd58528a9b7354cb5be3f618b791b5469a95d8f2
ansible-tower <pcwadmin2016@163.com>
2017-03-22 10:24:38 +0800

    amend remove_outdate_backup.sh

--- CompanyServer/remove_outdate_backup.sh
+++ CompanyServer/remove_outdate_backup.sh
@@ -8,6 +8,7 @@ CAD_DB_DIR=/backup/cad_pcw365_com/database
 MOBILE_DB_DIR=/backup/m_pcw365_com
 STATS_DIR=/backup/code_pcw365_com
 GITLAB_DIR=/backup/aliyun_test/gitlab_data_backup
+SVN_DIR=/backup/svn_backup
 WEB_EXPIRE=4
 PCW_DB_EXPIRE=20
 CAD_DB_EXPIRE=7
@@ -15,6 +16,7 @@ CAD_VIPAPPS_EXPIRE=20
 MOBILE_DB_EXPIRE=20
 STATS_EXPIRE=7
 GITLAB_EXPIRE=5
+SVN_EXPIRE=5
 
 remove_pcw_backup()
         {
@@ -54,13 +56,18 @@ remove_stats_backup()
 remove_gitlab_backup()
 	{
 		cd $GITLAB_DIR
-		find ./ 	-mtime +${GITLAB_EXPIRE} -exec rm -f '{}' \;
+		find ./  -name "*tar*"	-mtime +${GITLAB_EXPIRE} -exec rm -f '{}' \;
 	}
 
+remove_svn_backup()
+	{
+		cd $SVN_DIR
+		find ./  -name "*.gz"	-mtime +${SVN_EXPIRE} -exec rm -f '{}' \;
+	}
 
 remove_pcw_backup
 remove_cad_backup
 remove_mobile_backup
 remove_stats_backup
 remove_gitlab_backup
-
+remove_svn_backup
