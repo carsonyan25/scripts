c7ca49702179bd0e8847e312fea630f774389d0f
ansible-tower <pcwadmin2016@163.com>
2017-04-19 17:13:18 +0800

    add call_api.py

---
+++ AliyunStats/call_api.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+#created by carson 
+# this script call api in stats.pcw365.com server
+
+import commands
+import pycurl
+
+
+# curl url to generate statistics
+class API:
+
+	def __init__(self):
+		pass
+	
+	#Runing statistics for cad series software everyday
+	def stats(self):
+		c= pycurl.Curl()
+		c.setopt(c.URL, 'http://stats.pcw365.com?validate=cadtest268')
+		c.setopt(c.VERBOSE, True)
+		c.perform()
+
+
+if __name__ == '__main__':
+	
+	opt=API()
+	opt.stats()
+
+else:
+	pass
