e154fe8eaffab6bb7efdbcde026c8b47ad4950e8
ansible-tower <pcwadmin2016@163.com>
2017-04-27 14:09:48 +0800

    add svn backup in copy_backups_to_rtx_server.sh

--- CompanyServer/copy_backups_to_rtx_server.sh
+++ CompanyServer/copy_backups_to_rtx_server.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 #created by carson
-# use to copy all server's database new backup to rtx.pcw365.com(f:\rtx_share\All_Backup) 
-
-
+# use to copy all server's database new backups , gitlab backup, svn backup  to rtx.pcw365.com(f:\rtx_share\All_Backup) 
 
 PCW_DB_DIR=/backup/www_pcw365_com/databases/fullbackup
 CAD_DB_DIR=/backup/cad_pcw365_com/database/fullbackup
 MOBILE_DB_DIR=/backup/m_pcw365_com/database/
 ALIYUNTEST_DIR=/backup/aliyun_test/gitlab_data_backup/
-RTX_DIR=/rtx_share/All_Backup
 STATS_DB_DIR=/backup/stats.pcw365.com/stats.pcw365.com_backup
 USA_CAD_DB_DIR=/backup/usa.pcw365.com/usa.pcw365.com_backup
+SVN_BACKUP_DIR=/backup/svn_backup
 
 # RTX_DIR is locate in rtx.pcw365.com(f:\rtx_share\All_Backup) 
 RTX_DIR=/rtx_share/All_Backup
-curdate=`date +%Y%m%d`
-speed=30000
+speed=50000
 
 #delete old backup file in RTX_DIR
 rm -f  $RTX_DIR/*.* 
@@ -26,7 +23,5 @@ find ${MOBILE_DB_DIR} -name "*.gz" -mtime 0 -exec rsync -avpP --bwlimit=${speed}
 find ${ALIYUNTEST_DIR} -name "*.tar*" -mtime 0 -exec rsync -avpP --bwlimit=${speed} '{}'  ${RTX_DIR} \;
 find ${STATS_DB_DIR} -name "*.gz" -mtime 0 -exec rsync -avpP --bwlimit=${speed} '{}' ${RTX_DIR} \; 
 find ${USA_CAD_DB_DIR} -name "*.gz" -mtime 0 -exec rsync -avpP --bwlimit=${speed} '{}' ${RTX_DIR} \; 
-
-
-
+find ${SVN_BACKUP_DIR} -name "*.gz" -mtime 0 -exec rsync -avpP --bwlimit=${speed} '{}' ${RTX_DIR} \; 
 
